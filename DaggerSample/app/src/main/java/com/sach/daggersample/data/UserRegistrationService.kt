9a1a75d1888f29bc8c5f5f342a4bb28dc66bbb12
Sachin Shelar <sachrohile@gmail.com>
2023-10-11 12:23:26 +0530

    @Name qualifiers

--- DaggerSample/app/src/main/java/com/sach/daggersample/data/UserRegistrationService.kt
+++ DaggerSample/app/src/main/java/com/sach/daggersample/data/UserRegistrationService.kt
@@ -2,13 +2,14 @@ package com.sach.daggersample.data
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Named
 
 class UserRegistrationService @Inject constructor(
     private val userRepository: UserRepository,
-    private val service: NotificationService
+    @Named("sms") private val service: NotificationService
 ) {
 
-    fun registerUser(username:String, password:String) {
+    fun registerUser(username: String, password: String) {
         Log.e(Constant.TAG, "New User Registration - $username & $password")
         userRepository.saveUser()
         service.send()
--- DaggerSample/app/src/main/java/com/sach/daggersample/data/module/ServiceModule.kt
+++ DaggerSample/app/src/main/java/com/sach/daggersample/data/module/ServiceModule.kt
@@ -2,15 +2,24 @@ package com.sach.daggersample.data.module
 
 import com.sach.daggersample.data.EmailService
 import com.sach.daggersample.data.NotificationService
+import com.sach.daggersample.data.SmsService
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class ServiceModule {
 
+    @Named("email")
     @Provides
     fun provideService(): NotificationService {
         return EmailService()
+    }
+
+    @Named("sms")
+    @Provides
+    fun provideSmsService(): NotificationService{
+        return SmsService()
     }
 
 
